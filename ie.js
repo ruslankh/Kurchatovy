6f84dcad95acfc1953df79585dd6de2354225050
ruslankh <foraccounts@outlook.com>
2013-10-13 21:48:18 +0400

    22

--- ie.js
+++ ie.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $("#alert").append("Ваш браузер устарел! Очень устарел. <a href='http://chrome.google.com/'>Обновить</a>");
+    $("#alert").append("Ваш браузер устарел! Очень устарел.&nbsp; <a href='http://chrome.google.com/' style='color: #ffffff'>Обновить&nbsp;</a><a href='http://enoughie6.com/'style='color: #ffffff'>(почему это важно)</a> ");
 });
