37dbb5fdbefec80f3e946a654aeaec9b8d02f3be
ruslankh <foraccounts@outlook.com>
2013-10-13 22:00:29 +0400

    1234

--- ie.js
+++ ie.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $("#alert").prepend("<div id='alertie'>"Ваш браузер устарел! Очень устарел.&nbsp; <a href='http://chrome.google.com/' style='color: #ffffff'>Обновить&nbsp;</a><a href='http://enoughie6.com/'style='color: #ffffff'>(почему это важно)</a></div> ");
+    $("#alert").prepend("<div id='alertie'>Ваш браузер устарел! Очень устарел.&nbsp; <a href='http://chrome.google.com/' style='color: #ffffff'>Обновить&nbsp;</a><a href='http://enoughie6.com/'style='color: #ffffff'>(почему это важно)</a></div> ");
 });
