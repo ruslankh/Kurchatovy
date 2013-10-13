1bac23bad25b17bddcf27af0c1328288d0f54ca6
ruslankh <foraccounts@outlook.com>
2013-10-13 21:19:18 +0400

    11

--- ie.js
+++ ie.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $("#wrapper").empty().prepend('<div id="ie">Ваш браузер устарел<br><a href="http://chrome.google.com/">Установите Google Chrome</a> или любой другой современный браузер </div>');
+    $("#alert").append("Ваш браузер устарел!");
 });
